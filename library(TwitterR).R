1eabc1e1e3f9f57e4e6471fed8af373f1ed1ca52
VictorManuelMorenoJimenez <victormmorenojimenez@gmail.com>
2016-03-17 13:56:07 +0100

    probando

---
+++ library(TwitterR).R
@@ -0,0 +1,6 @@
+# Usar el paquete twitter
+library(twitteR)
+# Conseguir autorización de la app de twitter
+setup_twitter_oauth("mbL0ZaTzMFg4i9RmUf42vRic7","8jDW2ushgg9OSO0jE3fCF3r6Uamg0s9EnW2BOrR3BmTnup0IqM", "4566942261-nMnf4iU0PBYPKZxwIBczDRYKi0s0FMSLjdilKyO", "GoYnfBCqzzmPIQrV0VaG7NAbsQr0rJ8SsGpaJwrgUK40D")
+# Objeto de búsqueda
+searchTwitteR("Pablo Iglesias")
