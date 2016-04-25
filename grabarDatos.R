f979d1b7f8826740e0e56667b27662d3ed8d843f
VictorManuelMorenoJimenez <victormmorenojimenez@gmail.com>
2016-04-25 19:49:52 +0200

    archivo para compartir

--- grabarDatos.R
+++ grabarDatos.R
@@ -1,9 +1,10 @@
 library(twitteR)
 
 #Extraer texto
-textoTweets <- function(nf, fichero) {
+textoTweets <- function(fichero, dir="datos") {
+  nf <- sprintf("%s/%s", dir, fichero)
   listTweets <- readRDS(nf)
-  #sapply(listTweets, grabaTextoTweet)
+  sapply(listTweets, grabaTextoTweet)
 }
 
 grabaTextoTweet <- function(tweet, directorio= "txt") {
@@ -12,4 +13,10 @@ grabaTextoTweet <- function(tweet, directorio= "txt") {
     nf <- sprintf("%s/%s.txt", directorio, tweet$id)
     writeLines(tweet$text, nf)
   }
+}
+
+grabaTodosDatosTxt <- function(dirDatos="Datos", dirTxt="txt")
+{
+  ficheros <- list.files(dirDatos)
+  sapply(ficheros, function(x) textoTweets(x, dirDatos))
 }
